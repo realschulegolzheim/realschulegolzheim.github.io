7256ec931ff0d5a0d480ea86b0222a9efccdcc36
Christian Geuer-Pollmann <christian@geuer-pollmann.de>
2014-08-24 23:08:03 +0200

    tweaked paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,26 +3,25 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		'pkg': grunt.file.readJSON("package.json"),
 		'clean': {
-			output: [ '_site/*']
+			'output' : [ '_site/*']
 		},
-
-		'jekyll': {
-			dist: {
-				options: {
-					config: '_config.yml'
+		'jekyll' : {
+			'dist' : {
+				'options' : {
+					'config' : '_config.yml'
 				}
 			}
 		},
-		'ftpush': {
-			prod: {
-				auth: {
-					host: 'schulen.duesseldorf.de',
-					port: 21,
-					authKey: 'schulenduesseldorfde'
+		'ftpush' : {
+			'prod' : {
+				'auth' : {
+					'host' : 'schulen.duesseldorf.de',
+					'port' : 21,
+					'authKey' : 'schulenduesseldorfde'
 				},
-				src: '_site',
-				dest: '/httpdocs/v2',
-				exclusions: [
+				'src' : '_site',
+				'dest' : '/httpdocs/v2',
+				'exclusions' : [
 					'path/to/source/folder/**/.DS_Store', 
 					'path/to/source/folder/**/Thumbs.db', 
 					'path/to/dist/tmp'
@@ -34,6 +33,7 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks("grunt-contrib-clean");
 	grunt.loadNpmTasks("grunt-jekyll");
 	grunt.loadNpmTasks("grunt-ftp-deploy");
+	grunt.loadNpmTasks("grunt-ftpush");
 
 	grunt.registerTask("default", ["clean"]);
 	grunt.registerTask("jekyll", ["jekyll:dist"]);
---
+++ build.cmd
@@ -0,0 +1,2 @@
+chcp 65001
+call jekyll build --trace
